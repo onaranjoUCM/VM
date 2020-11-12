134eb7e0c6ed411d44cde8252e1fb19359b31bec
onaranjoUCM <onaranjo@ucm.es>
2020-11-12 19:13:23 +0100

    Engines created

---
+++ Practica1/desktopEngine/src/main/java/es/ucm/gdv/engine/desktop/Engine.java
@@ -0,0 +1,23 @@
+package es.ucm.gdv.engine.desktop;
+
+import java.io.InputStream;
+
+import es.ucm.gdv.engine.Font;
+import es.ucm.gdv.engine.Graphics;
+
+public class Engine implements es.ucm.gdv.engine.Engine {
+    @Override
+    public Graphics getGraphics() {
+        return null;
+    }
+
+    @Override
+    public Font getFont() {
+        return null;
+    }
+
+    @Override
+    public InputStream openInputStream(String filename) {
+        return null;
+    }
+}
