eb8c00676323582c7ec2bdf0cd85fde38a367b36
GonzaPM7 <gonzap03@ucm.es>
2020-11-26 19:47:41 +0100

    move enemy, lack of waiting time

--- Practica1/offTheLineLogic/src/main/java/es/ucm/gdv/offtheline/Enemy.java
+++ Practica1/offTheLineLogic/src/main/java/es/ucm/gdv/offtheline/Enemy.java
@@ -14,29 +14,87 @@ public class Enemy extends GameObject {
     private float offsetY_;
     private float time1_;
     private float time2_;
+    private double dirX, dirY;
+    private float speedMove_;
 
     public Enemy(float posX, float posY, int length, float angle, float speed, float offsetX, float offsetY, float time1, float time2){
         super(posX, posY, length, 0);
         length_ = length;
         speed_ = speed;
         angle_ = angle;
-        offsetX_ = offsetX;
-        offsetY_ = offsetY;
         time1_ = time1;
         time2_ = time2;
         posXIni = posX;
-        posXIni = posY;
+        posYIni = posY;
+        if(offsetX != 0 || offsetY!= 0) {
+            offsetX_ = posX_ + offsetX;
+            offsetY_ = posY_ + offsetY;
+            int x = ((int)offsetX_ - (int)posX_)^2 + ((int)offsetY_ - (int)posY_)^2;
+            float magnitude = (float)Math.sqrt(x);
+            speedMove_ = magnitude/time1_;
+        }
     }
 
     @Override
     public void update(double deltaTime) {
+        if(offsetX_ != 0 || offsetY_!= 0) {
+            updateDirection();
+            posX_ += speedMove_*deltaTime * dirX;
+            posY_ += speedMove_* deltaTime * dirY;
+        }
         angle_ += speed_;
+    }
+
+    private void updateDirection() {
+
+        if (posX_ == offsetX_ && posY_ == offsetY_) {
+            float auxX = offsetX_;
+            float auxY = offsetY_;
+            offsetX_ = posXIni;
+            offsetY_ = posYIni;
+            posXIni = auxX;
+            posYIni = auxY;
+        }
+        // Calculate direction
+        float x1 = posX_;
+        float y1 = posY_;
+        float x2 = offsetX_;
+        float y2 = offsetY_;
+
+        float num = (y2 - y1);
+        float den = (x2 - x1);
+
+        double angle;
+        if (num > 0 && den == 0) {
+            angle = 90.0;
+            dirX = 0;
+            dirY = 1;
+        } else if (num == 0 && den < 0) {
+            angle = 180;
+            dirX = -1;
+            dirY = 0;
+        } else if (num < 0 && den == 0) {
+            angle = 270.0;
+            dirX = 0;
+            dirY = -1;
+        } else if (num == 0 && den > 0) {
+            angle = 0;
+            dirX = 1;
+            dirY = 0;
+        } else{
+            angle = Math.toDegrees(Math.atan(num / den));
+            dirX = Math.cos(Math.toRadians(angle));
+            dirY = Math.sin(Math.toRadians(angle));
+        }
+
     }
 
     @Override
     public void render(Graphics g) {
         g.setColor(255, 0, 0);
-        g.drawLine((int) posX_, (int) posY_, (int) (posX_ + Math.cos(angle_) * (W_ / 2)), (int) (posY_ - Math.sin(angle_) * (W_ / 2))); //Linea1
-        g.drawLine((int) posX_, (int) posY_, (int) (posX_ - Math.cos(angle_) * (W_ / 2)), (int) (posY_ + Math.sin(angle_) * (W_ / 2))); //Linea2
+        int x = (int) (posX_ + Math.cos(angle_) * (W_ / 2));
+        int y =(int) (posY_ - Math.sin(angle_) * (W_ / 2));
+        g.drawLine((int) posX_, (int) posY_, (int) (posX_ + Math.cos(Math.toRadians (angle_)) * (W_ / 2)), (int) (posY_ - Math.sin(Math.toRadians (angle_)) * (W_ / 2))); //Linea1
+        g.drawLine((int) posX_, (int) posY_, (int) (posX_ - Math.cos(Math.toRadians (angle_)) * (W_ / 2)), (int) (posY_ + Math.sin(Math.toRadians (angle_)) * (W_ / 2))); //Linea2
     }
 }
