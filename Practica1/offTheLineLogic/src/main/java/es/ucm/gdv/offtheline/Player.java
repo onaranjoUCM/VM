a9fecba4b53d879d41e599cfa4bcb9fc546e1156
onaranjoUCM <onaranjo@ucm.es>
2020-11-30 11:41:28 +0100

    Fixed player flying away

--- Practica1/offTheLineLogic/src/main/java/es/ucm/gdv/offtheline/Player.java
+++ Practica1/offTheLineLogic/src/main/java/es/ucm/gdv/offtheline/Player.java
@@ -10,6 +10,7 @@ public class Player extends GameObject {
     private double dirX, dirY;
     private Path currentPath_;
     int pathVertexIndex = 0;
+    int nextVertexIndex = 1;
 
     public Player(Path path, int W, int H, float speed, float angle){
         super(path.vertices.get(0)[0], path.vertices.get(0)[1], W, H);
@@ -22,12 +23,23 @@ public class Player extends GameObject {
 
     @Override
     public void update(double deltaTime) {
-        if (currentPath_ != null)
-            updateDirection();
+        // If on a path, follow it
+        updateDirection();
 
+        // Update position
         posX_ += moveSpeed_ * deltaTime * dirX;
         posY_ += moveSpeed_ * deltaTime * dirY;
 
+        // If the player skips the target, return to it
+        if (skippedTarget()) {
+            posX_  = currentPath_.vertices.get(nextVertexIndex)[0];
+            posY_ = currentPath_.vertices.get(nextVertexIndex)[1];
+        }
+
+        // Calculate next target vertex
+        updateCurrentVertex();
+
+        // Update spin angle
         angle_ += speed_;
         angle2_+= speed_;
     }
@@ -46,38 +58,63 @@ public class Player extends GameObject {
     }
 
     private void updateDirection() {
-        int nextVertexIndex;
-        if (pathVertexIndex + 1 < currentPath_.vertices.size())
-            nextVertexIndex = pathVertexIndex + 1;
-        else
-            nextVertexIndex = 0;
+        if (currentPath_ != null) {
+            if (pathVertexIndex + 1 < currentPath_.vertices.size())
+                nextVertexIndex = pathVertexIndex + 1;
+            else
+                nextVertexIndex = 0;
 
-        // Calculate direction
-        float x1 = currentPath_.vertices.get(pathVertexIndex)[0];
-        float y1 = currentPath_.vertices.get(pathVertexIndex)[1];
-        float x2 = currentPath_.vertices.get(nextVertexIndex)[0];
-        float y2 = currentPath_.vertices.get(nextVertexIndex)[1];
+            // Calculate direction
+            float x1 = currentPath_.vertices.get(pathVertexIndex)[0];
+            float y1 = currentPath_.vertices.get(pathVertexIndex)[1];
+            float x2 = currentPath_.vertices.get(nextVertexIndex)[0];
+            float y2 = currentPath_.vertices.get(nextVertexIndex)[1];
 
-        float num = (y2 - y1);
-        float den = (x2 - x1);
+            float num = (y2 - y1);
+            float den = (x2 - x1);
 
-        double angle;
-        angle = Math.toDegrees(Math.atan(num / den));
+            double angle;
+            angle = Math.toDegrees(Math.atan(num / den));
 
-        if ((x2 < x1))
-            angle += 180;
+            if ((x2 < x1))
+                angle += 180;
 
-        dirX = Math.cos(Math.toRadians(angle));
-        dirY = Math.sin(Math.toRadians(angle));
+            dirX = Math.cos(Math.toRadians(angle));
+            dirY = Math.sin(Math.toRadians(angle));
+        }
+    }
 
-        if (Math.abs(posX_ - currentPath_.vertices.get(nextVertexIndex)[0]) < 0.5 &&
-                Math.abs(posY_ - currentPath_.vertices.get(nextVertexIndex)[1]) < 0.5) {
-            pathVertexIndex++;
-            if (pathVertexIndex == currentPath_.vertices.size()) pathVertexIndex = 0;
+    private void updateCurrentVertex() {
+        if (currentPath_ != null) {
+            if (Math.abs(posX_ - currentPath_.vertices.get(nextVertexIndex)[0]) < 0.5 &&
+                    Math.abs(posY_ - currentPath_.vertices.get(nextVertexIndex)[1]) < 0.5) {
+                pathVertexIndex++;
+                if (pathVertexIndex == currentPath_.vertices.size()) pathVertexIndex = 0;
+            }
         }
     }
 
     public boolean isFlying() {
         return currentPath_ == null;
+    }
+
+    private boolean skippedTarget() {
+        if (currentPath_ != null) {
+            float originX = currentPath_.vertices.get(pathVertexIndex)[0];
+            float originY = currentPath_.vertices.get(pathVertexIndex)[1];
+            float targetX = currentPath_.vertices.get(nextVertexIndex)[0];
+            float targetY = currentPath_.vertices.get(nextVertexIndex)[1];
+
+            if (originX < targetX)
+                return posX_ > targetX;
+            else if (originX > targetX)
+                return posX_ < targetX;
+            else if (originY < targetY)
+                return posY_ > targetY;
+            else if (originY > targetY)
+                return posY_ < targetY;
+        }
+
+        return false;
     }
 }
