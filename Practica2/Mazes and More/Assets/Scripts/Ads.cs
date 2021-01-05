b6b887474d0b45c6d159725d0591c2572b5800b0
GonzaPM7 <gonzap03@ucm.es>
2021-01-05 15:32:27 +0100

    Añadido anuncios para ser ricos

---
+++ Practica2/Mazes and More/Assets/Scripts/Ads.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour
+{
+    string gameId = "3963369";
+    bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+    }
+
+    public void ShowInterstitialAd()
+    {
+        // Check if UnityAds ready before calling Show method:
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+        else
+        {
+            Debug.Log("Interstitial ad not ready at the moment! Please try again later!");
+        }
+    }
+
+    public void ShowRewardedVideo(string placement)
+    {
+        // Check if UnityAds ready before calling Show method:
+        if (Advertisement.IsReady(placement))
+        {
+            Advertisement.Show(placement);
+        }
+        else
+        {
+            Debug.Log("Rewarded video is not ready at the moment! Please try again later!");
+        }
+    }
+
+    // Implement IUnityAdsListener interface methods:
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        // Define conditional logic for each ad completion status:
+        if (showResult == ShowResult.Finished)
+        {
+            // Reward the user for watching the ad to completion.
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            Debug.LogWarning("SKIPEADO");
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            Debug.LogWarning("The ad did not finish due to an error.");
+        }
+    }
+}
